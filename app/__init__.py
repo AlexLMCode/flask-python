786ab90239526d4e4a39aac091dfcdca2914cda5
AlexLMCode <alexlujanm@outlook.com>
2021-03-28 13:01:09 -0600

    Fixed a typo in 404.html

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,18 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from .views import page
 from flask_wtf.csrf import CSRFProtect
+from .views import page
 
 app = Flask(__name__)
+bootstrap = Bootstrap()
 
 csrf = CSRFProtect()
-bootstrap = Bootstrap()
 
 
 def create_app(config):
     app.config.from_object(config)
-
-    csrf.init_app(app)
-
     bootstrap.init_app(app)
+    csrf.init_app(app)
 
     app.register_blueprint(page)
 
